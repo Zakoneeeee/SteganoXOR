4169b789da06c6d0ab4090da92bf1b2614f4b8b8
Zakoneeeee <valenmiskah@gmail.com>
2026-07-29 17:22:34 +0700

    bypass public folder

--- resources/views/stegano/index.blade.php
+++ resources/views/stegano/index.blade.php
@@ -12,6 +12,7 @@
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="/public/css/style.css">
 </head>
 <body>
 
@@ -128,6 +129,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <script src="/js/script.js"></script>
+    <script src="/public/js/script.js"></script>
     
     <script>
         // Logika sederhana untuk pindah tab area Encode dan Decode
